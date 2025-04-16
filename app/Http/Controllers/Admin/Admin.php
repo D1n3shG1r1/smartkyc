359c4d282b44fb71d7288a1ea39c4274b83f78f2
D1n3shG1r1 <dineshleadtech@gmail.com>
2025-04-16 22:10:13 +0530

    customerportal debug16apr2025

--- app/Http/Controllers/Admin/Admin.php
+++ app/Http/Controllers/Admin/Admin.php
@@ -745,7 +745,9 @@ class Admin extends Controller
                 $applications = $applicationsObj->toArray();
                
                 foreach($applications["data"] as &$row){
-                    $row["verificationOutcomeTxt"] = verificationStatusTxt($row["verificationOutcome"]);
+                    //$row["verificationOutcomeTxt"] = verificationStatusTxt($row["verificationOutcome"]);
+
+                    $row["verificationOutcomeTxt"] = $row["verificationOutcome"];
 
                     $customer = Customers_model::select("fname", "lname")->where("id", $row["customerId"])->first();
                     $row["customerName"] = ucwords($customer["fname"] . " " . $customer["lname"]);
